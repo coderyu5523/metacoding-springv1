481664fc54ae38b037b413b75347f93b94a3bd4c
coderyu5523 <coderyu5523@naver.com>
2025-08-28 23:50:03 +0900

    chapter 완성

---
+++ chap01/html/src/main/java/com/metacoding/springv1/board/BoardController.java
@@ -0,0 +1,29 @@
+package com.metacoding.springv1.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    
+    @GetMapping("/boards/save-form")
+    public String saveForm() {
+        return "board/save-form";
+    }
+
+
+    @GetMapping("/boards/1")
+    public String detail() {
+        return "board/detail";
+    }
+
+    @GetMapping("/boards/1/update-form")
+    public String updateForm() {
+        return "board/detail-form";
+    }
+}
